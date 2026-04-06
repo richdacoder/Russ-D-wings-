224cfed1ab34f19a25e925158ea9b7201eacfee9
richard williams <richard.williams5697@yahoo.com>
2026-04-06 10:40:42 -0400

    layout and home page are displaying

--- apps/admin/src/app/layout.jsx
+++ apps/admin/src/app/layout.jsx
@@ -1,21 +1,27 @@
+"use client";
+
 import { useState, useEffect } from "react";
-import HomePage from "./page.jsx";
 
-function Layout(){
+export default function  RootLayout( {children} ){
 
-}
+
 return(
 
-<html lang="en">
+<html >
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
 </head>
 <body>
-<HomePage/>
+  <header>
+
+  </header>
+<footer>
+<main>
+{children}
+  </main>
+  </footer>
 </body>
 </html>
 
 
 )
+}
--- apps/admin/src/app/page.jsx
+++ apps/admin/src/app/page.jsx
@@ -1,11 +1,14 @@
+"use client";
+
 import { useState, useEffect } from "react";
 
 
 export default function HomePage() {
 
-}
+
 return(
   <>
   <div>working</div>
   </>
 )
+}
