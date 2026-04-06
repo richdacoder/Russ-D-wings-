cc5d7a2f65720caba1df9ec0358b46101cbdfc89
richard williams <richard.williams5697@yahoo.com>
2026-04-06 09:36:00 -0400

    added npm to admin folder

--- apps/admin/src/app/layout.jsx
+++ apps/admin/src/app/layout.jsx
@@ -1,11 +1,21 @@
 import { useState, useEffect } from "react";
 import HomePage from "./page.jsx";
 
-function Layout (){
+function Layout(){
 
 }
 return(
-  <>
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Document</title>
+</head>
+<body>
 <HomePage/>
-  </>
+</body>
+</html>
+
+
 )
