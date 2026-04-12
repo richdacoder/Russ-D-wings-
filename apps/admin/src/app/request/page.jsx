a4934cb364adbf0b49f6739393e74ce34a55c40e
richard williams <richard.williams5697@yahoo.com>
2026-04-12 18:51:01 -0400

    change button content

--- apps/admin/src/app/request/page.jsx
+++ apps/admin/src/app/request/page.jsx
@@ -1,6 +1,23 @@
 "use client";
 
 export default function Request(){
+  /*
+- request doesnt need checking if exist system because he can take more than two orders
+  in one hour
+
+  - so basically is just yes or no of he is available or have enough wings to cook in the moment
+
+  flow:
+  - list of requests will pop up
+  -  click name
+  - information of order will pop up you will hit accept they will get text or email on
+    either if their order have been accepted or not and message them on what can be done.
+
+  - request gets deleted
+
+
+
+  */
   return(
     <div>working yeah</div>
   )
--- apps/admin/src/app/schedule/page.jsx
+++ apps/admin/src/app/schedule/page.jsx
@@ -1,7 +1,42 @@
 "use client";
 
 export default function Schedule(){
+/*
+- most likely a form
+
+- choose a time block between certain times
+  in the same day(since thats what request is for
+    creating a weekly schedule is kinda redondent)
+
+ - how to create a time listing selection
+*/
   return(
-    <div>works yeah</div>
+    <div>
+      <h1>
+        Create Time Blocks
+      </h1>
+      <form>
+      <div>
+        <label>
+          Start Time
+        </label>
+        <input
+        type="time">
+        </input>
+      </div>
+
+      <div>
+        <label>
+          End Time
+        </label>
+        <input
+        type="time">
+        </input>
+      </div>
+
+      <button type="submit"> Add Time Block </button>
+      </form>
+
+    </div>
   )
 }
