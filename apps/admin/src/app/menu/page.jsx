0aedbdfa310b0d4579cb734b66be936aa8ce9196
richard williams <richard.williams5697@yahoo.com>
2026-04-13 16:04:04 -0400

    added options for catergory

--- apps/admin/src/app/menu/page.jsx
+++ apps/admin/src/app/menu/page.jsx
@@ -1,7 +1,33 @@
 "use client";
 
+import {useState, useEffect} from "react";
+
 export default function Menu(){
+/*
+
+** there would be get post and delete on this page
+
+- catergory
+- inside of input option selection will drop down (GET) with what was added
+
+- dish will be an input
+
+*/
   return(
-    <div>working yeah</div>
+    <div>
+      <h1>Menu</h1>
+      <form>
+        <div>
+        <label>Catergory</label>
+        <select>
+            <option>Main</option>
+            <option>Side</option>
+            <option>Drink</option>
+        </select>
+        </div>
+
+
+      </form>
+      </div>
   )
 }
