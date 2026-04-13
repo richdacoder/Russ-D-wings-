1bdd3043e6e5a12ecb226dbd4dea2ffd362d88f0
richard williams <richard.williams5697@yahoo.com>
2026-04-13 13:01:31 -0400

    added more to homepage admin

--- apps/admin/src/app/page.jsx
+++ apps/admin/src/app/page.jsx
@@ -8,7 +8,18 @@ export default function HomePage() {
 
 return(
   <>
-  <div>working</div>
+  <div>
+  <h1>orders</h1>
+  <span>Date</span>
+  <span>Name</span>
+  <span>Time</span>
+
+  <button>
+    <span></span>
+    <span></span>
+    <span></span>
+  </button>
+  </div>
   </>
 )
 }
