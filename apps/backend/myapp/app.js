700155c31f6c846cfcda4fb08719c0c17cc71b3f
richard williams <richard.williams5697@yahoo.com>
2026-04-20 17:12:22 -0400

    added status checker

--- apps/backend/myapp/app.js
+++ apps/backend/myapp/app.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const app = express();
+
+/*
+- make sure health is 200
+- added allowed urls
+- add cors
+- make middleware
+- make 404 handler
+- make error hanler
+
+*/
+
+app.get('/health', (req, res) => {
+  res.status(200)
+  .json({status:200})
+})
