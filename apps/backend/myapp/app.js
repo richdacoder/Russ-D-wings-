24540a93232779be7a3f3123ec830dd71a91ea7c
richard williams <richard.williams5697@yahoo.com>
2026-04-23 12:04:34 -0400

    added router.post to availability.js

--- apps/backend/myapp/app.js
+++ apps/backend/myapp/app.js
@@ -38,16 +38,6 @@ app.use(
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 
-
-  const route =
-  router.post('/availability', (req, res) => {
-     console.log(req.body);
-    return res.status(200).json({
-      available: true,
-      message: 'Test working',
-    })
-  });
-
   app.use('/api', route);
 
 console.log('check', route);
