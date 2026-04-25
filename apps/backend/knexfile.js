be06f648f8aad47d39c30ccbde331633a95464f7
richard williams <richard.williams5697@yahoo.com>
2026-04-25 10:02:15 -0400

    added connection info

--- apps/backend/knexfile.js
+++ apps/backend/knexfile.js
@@ -2,7 +2,11 @@ module.exports = {
 developement:{
   client:'pg',
   connection:{
-
+    host: "postgres-db",
+    port: 5432,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB
   }
 }
 }
