b559d4c26186064a6771cafec31bd38852a0c726
richard williams <richard.williams5697@yahoo.com>
2026-04-27 10:38:48 -0400

    added the table in in create time slot migration page

--- apps/backend/migrations/migrations/20260427143622_create_time_slots.js
+++ apps/backend/migrations/migrations/20260427143622_create_time_slots.js
@@ -2,14 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('time_slots', (table) => {
+    table.uuid('id').primary();
+
+    table.date('date').notNullable();          // schedule day
+    table.time('start_time').notNullable();    // start time
+    table.time('end_time').notNullable();      // end time
+
+    table.boolean('is_active').defaultTo(true);
+
+    table.timestamps(true, true); // created_at + updated_at
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('time_slots');
 };
