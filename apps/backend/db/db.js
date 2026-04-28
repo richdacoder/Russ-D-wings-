ac654ccb9c08e5f4209859f09eb49c8248dbde6e
richard williams <richard.williams5697@yahoo.com>
2026-04-28 00:50:14 -0400

    add env console.log

--- apps/backend/db/db.js
+++ apps/backend/db/db.js
@@ -1,4 +1,6 @@
 const knex = require('knex');
 const knexfile = require('../knexfile.js');
 
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
+
+console.log(`🚀 Database connecting to: ${env} mode`);
