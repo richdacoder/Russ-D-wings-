fe6811f061d168cbce3596ff9466fcbb7566d87a
richard williams <richard.williams5697@yahoo.com>
2026-04-28 10:46:05 -0400

    turned req.body to data

--- apps/backend/routes/availability.js
+++ apps/backend/routes/availability.js
@@ -2,10 +2,29 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/db.js');
 
+/*
+- put rewquest body in variable
+
+- create new object exracting from request body
+
+- make an array named required for required data or any other way to make sure data is valid
+  and none is missing.
+
+  -
+
+-
+
+*/
 console.log('router');
 
 router.post('/availability', async (req, res) => {
-  console.log(req.body);
+  const data = req.body
+  console.log(data);
+  try{
+
+  }catch(err){
+    console.error(err);
+  }
 });
 
 module.exports = router
