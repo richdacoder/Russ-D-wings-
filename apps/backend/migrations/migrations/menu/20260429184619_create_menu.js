da2a5ed10c534b517d02f1436aee72edd2276baf
richard williams <richard.williams5697@yahoo.com>
2026-04-29 16:26:52 -0400

    added table name

--- apps/backend/migrations/migrations/menu/20260429184619_create_menu.js
+++ apps/backend/migrations/migrations/menu/20260429184619_create_menu.js
@@ -1,15 +1,16 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
  */
+
 exports.up = function(knex) {
-  
+knex.schema.createTable('menu', (table) => {
+
+})
 };
 
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+
 };
