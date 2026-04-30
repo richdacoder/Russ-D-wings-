2c6e29ddb25b0b63754d53b1486801550cc78489
richard williams <richard.williams5697@yahoo.com>
2026-04-30 11:33:08 -0400

    made require array in menu.js

--- apps/backend/routes/menu.js
+++ apps/backend/routes/menu.js
@@ -2,10 +2,25 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/db.js');
 
+/*
+- make require array
+- check for missing field
+- make 404 error
+- insert into database
+
+
+*/
 router.post("/menu", async (req,res) =>{
   const data = req.body;
   console.log('menu', data);
 try{
+const require = [
+   'category',
+   'dish',
+   'price',
+   'stock',
+   'description'
+];
 
 }catch(err){
 console.error(err);
