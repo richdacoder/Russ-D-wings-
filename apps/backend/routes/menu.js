0e3175af97815e569d30f9fe80ea802cda71c10e
richard williams <richard.williams5697@yahoo.com>
2026-04-30 00:23:23 -0400

    added router post for menu

--- apps/backend/routes/menu.js
+++ apps/backend/routes/menu.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const router = express.router();
 const db = require('../db/db.js');
+
+router.post("/menu", async (req,res) =>{
+try{
+
+}catch(err){
+console.error(err);
+}
+})
