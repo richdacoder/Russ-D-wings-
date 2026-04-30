1619508e0fb2edd7c4b70618742073828504267f
richard williams <richard.williams5697@yahoo.com>
2026-04-30 00:06:57 -0400

    started menu table

--- apps/backend/migrations/migrations/menu/20260429184619_create_menu.js
+++ apps/backend/migrations/migrations/menu/20260429184619_create_menu.js
@@ -2,15 +2,21 @@
  * @param { import("knex").Knex } knex
  */
 
-exports.up = function(knex) {
-knex.schema.createTable('menu', (table) => {
-
-})
+exports.up = function (knex) {
+  return knex.schema.createTable('menu', (table) => {
+    table.increments('id').primary();
+    table.string('category').notNullable();
+    table.string('dish').notNullable();
+    table.decimal('price', 10, 2).notNullable();
+    table.boolean('stock').defaultTo(true);
+    table.text('description');
+    table.timestamps(true, true);
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  */
-exports.down = function(knex) {
-
+exports.down = function (knex) {
+  return knex.schema.dropTable('menu');
 };
