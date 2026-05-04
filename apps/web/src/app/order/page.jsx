725839bf7456489fb559d234e8d06bae20a97fa3
richard williams <richard.williams5697@yahoo.com>
2026-05-04 12:16:41 -0400

    added dish

--- apps/web/src/app/order/page.jsx
+++ apps/web/src/app/order/page.jsx
@@ -2,6 +2,38 @@
 
 import { useState, useEffect } from "react";
 
-export default function Order(){
+/*
+- create construct varibales
+- make GET request for menu and availablity
+- make timslots display for sameday
+- make menu display
+- put them in the input with onchange
+- put them in an object
+- handle submit
+-----------------------------------------
 
+for catering use type to differient in code base all coding logic for catering done
+on next and express route
+
+*/
+export default function Order(){
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [dish, setDish] = useState("");
+  const [total, setTotal] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+
+
+
+
+return(
+  <div>
+    <form>
+
+
+    </form>
+  </div>
+)
 }
