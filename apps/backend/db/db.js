022de34cb90ca9eb892e3804ae07db2c047da263
richard williams <richard.williams5697@yahoo.com>
2026-06-09 17:06:34 -0400

    added local to env

--- apps/backend/db/db.js
+++ apps/backend/db/db.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const knexfile = require('../knexfile.js');
 
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV || 'local';
 
 console.log(`🚀 Database connecting to: ${env} mode`);
 
