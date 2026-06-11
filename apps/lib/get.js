48c7826ba5f2dad92a3d6affe62ce8ea906b2595
richard williams <richard.williams5697@yahoo.com>
2026-06-11 15:18:01 -0400

    edited get by removing body from res json

--- apps/lib/get.js
+++ apps/lib/get.js
@@ -1,6 +1,6 @@
 /*
-- export function with aysnc
-- throw format data
+- export function with aysnc**
+- throw format data **
 - url variable to go in fetch
 - turn into json in fetch
 - make sure to solve if res is not there
@@ -12,6 +12,17 @@
 export async function get(formdata){
   const url = process.env.NEXT_PUBLIC_API_URL;
   console.log('url', url);
+
+  const res = await fetch(
+  `${url}/api/menu`,
+  {
+    method: 'GET',
+    }
+
+  )
+  const data = await res.json()
+
+
 
 
 }
