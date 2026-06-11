93e2a9de5f758e06c65954442d27dc43af7b85b8
richard williams <richard.williams5697@yahoo.com>
2026-06-10 20:33:44 -0400

    get function started

---
+++ apps/lib/get.js
@@ -0,0 +1,14 @@
+/*
+- export function with aysnc
+- throw format data
+- url variable to go in fetch
+- turn into json in fetch
+- make sure to solve if res is not there
+
+
+
+*/
+
+export async function get(){
+
+}
