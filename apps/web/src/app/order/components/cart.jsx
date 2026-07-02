337119ca86bd9f75e631159d6de66ab2d0451a7a
richard williams <richard.williams5697@yahoo.com>
2026-07-02 13:59:19 -0400

    added use state and cart variable to cart.jsx

--- apps/web/src/app/order/components/cart.jsx
+++ apps/web/src/app/order/components/cart.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import {useState} from 'react';
+
 /*
 add   const [quantity, setQuantity] = useState(0);
   const [itemQuantity, setItemQuantity] = useState({})
@@ -11,6 +13,7 @@ add   const [quantity, setQuantity] = useState(0);
 */
 
 export default function Cart(addToCart){
+  const [cart, setCart] = useState([]);
 
   console.log('see if addToCart works on cart', addToCart);
 
