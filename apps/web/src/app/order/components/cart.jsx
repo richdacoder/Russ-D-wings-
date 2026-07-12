8358de40db07df3c36e621424fc22c19fa7c3cc0
richard williams <richard.williams5697@yahoo.com>
2026-07-12 17:26:22 -0400

    _

--- apps/web/src/app/order/components/cart.jsx
+++ apps/web/src/app/order/components/cart.jsx
@@ -29,7 +29,7 @@ objective 7/10/26
 - move quantity back?xxx
 */
 
-export default function Cart({addToCart, setAddToCart, itemQuantity, etItemQuantity}){
+export default function Cart({addToCart, setAddToCart, itemQuantity, setItemQuantity}){
 
     console.log('item quantity', itemQuantity);
 
