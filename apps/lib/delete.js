d91e1b998e2001645706b21682776da94458a547
richard williams <richard.williams5697@yahoo.com>
2026-07-23 14:28:39 -0400

    added try and cathch

--- apps/lib/delete.js
+++ apps/lib/delete.js
@@ -1,10 +1,28 @@
-export default async function Delete (endPoint){
+/*
 
+objective finsh:
+- fnsih delete
+
+
+
+*/
+
+export default async function Delete (endPoint){
+try{
   const url = process.env.NEXT_PUBLIC_API_URL;
   console.log({
     'url': url,
     'endpoint': endPoint,
   });
 
+  fetch(() =>{
+
+
+  }
+
+  )
+} catch(err){
+
+}
 
 }
