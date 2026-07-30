5255c8d1aa0d0c3a3ea8e9dea5d215059ac0d4c1
richard williams <richard.williams5697@yahoo.com>
2026-07-30 16:08:54 -0400

    =

--- apps/admin/src/app/schedule/page.jsx
+++ apps/admin/src/app/schedule/page.jsx
@@ -139,7 +139,7 @@ console.log('block time start time', blockTime.some(time =>  endTime > time.star
                setShowAvailabilityManager(true);
     }else if(!overlapCheck){
       setContinueSubmit(true)
-      console.log("after", continueSubmit);
+      console.log("after", ontinueSubmit);
 
     };
 
