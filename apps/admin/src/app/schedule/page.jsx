5e12b03dc9a766e235e1b247c93cbc156ebfb852
richard williams <richard.williams5697@yahoo.com>
2026-08-01 23:45:32 -0400

    -

--- apps/admin/src/app/schedule/page.jsx
+++ apps/admin/src/app/schedule/page.jsx
@@ -116,14 +116,16 @@ console.log('block time start time', blockTime.some(time =>  endTime > time.star
   const overlapCheck = blockTime.some(time =>  endTime > time.start_time && startTime < time.end_time );
 
 const testOverLap = blockTime.map(time => {
-  const [, start] = time.start_time.split("T");
-  const [, end] = time.end_time.split("T");
+  const [, start] = time.start_time.split("T")[1].split(":").slice(0, 2);
+  const [day, end] = time.end_time;
 
   return {
     start,
-    end
+    end,
+    day
   };
 });
+console.log(testOverLap);
 console.log(testOverLap);
 
   const handleSubmit = async (e) => {
