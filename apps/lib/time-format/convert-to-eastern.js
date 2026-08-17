03f7adff9f3ecbf403d43a67963f47dd5fc58cba
richard williams <richard.williams5697@yahoo.com>
2026-08-17 14:05:38 -0400

    created function for convert to eastern

--- apps/lib/time-format/convert-to-eastern.js
+++ apps/lib/time-format/convert-to-eastern.js
@@ -1,4 +1,7 @@
-export default function ConvertToEastern(time){
+export default function ConvertToEastern(date){
 
-
+return date.toLocaleTimeString('en-US', {
+  timeZone: 'America/New_York'
+});
 }
+//move this function to schedule page
