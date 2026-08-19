8b2cd4c1d01fc6a6d925d1ab3e0e4236a1332bab
richard williams <richard.williams5697@yahoo.com>
2026-08-18 21:17:26 -0400

    now getting actually time

--- apps/lib/time-format/convert-to-eastern.js
+++ apps/lib/time-format/convert-to-eastern.js
@@ -1,7 +1,7 @@
 export default function ConvertToEastern(date){
 const convertedDate = new Date(date);
 
-return date.toLocaleTimeString('en-US', {
+return convertedDate.toLocaleTimeString('en-US', {
   timeZone: 'America/New_York'
 });
 }
