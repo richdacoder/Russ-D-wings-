c055b4dc19821bd561c734bf22004d764c143d65
richard williams <richard.williams5697@yahoo.com>
2026-08-26 17:53:20 -0400

    added time formatter

--- apps/admin/src/app/schedule/extras/available.jsx
+++ apps/admin/src/app/schedule/extras/available.jsx
@@ -2,12 +2,18 @@
 
 export default function Available({timeSlot, ConvertToEastern}){
   console.log('time slot', timeSlot);
+
+const timeFormat = (time) => {
+ const actualTime = ConvertToEastern(time, true).split(':');
+ return `${actualTime[0]}:${actualTime[1]} ${actualTime[2].split(' ')[1]}`;
+};
+
 return(
   <>
   <div>
     <h1>Start Time </h1>
       {timeSlot.map((time, index) =>(
-        <p key={index}>{ConvertToEastern(time.start_time, true)}</p>
+        <p key={index}>{timeFormat(time.start_time)}</p>
       )
       )}
           <h2>End Time </h2>
