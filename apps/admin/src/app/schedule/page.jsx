fd719a9d0b3d1585e8476ff2c84c1bdc37bb3a43
richard williams <richard.williams5697@yahoo.com>
2026-08-25 23:10:01 -0400

    -

--- apps/admin/src/app/schedule/page.jsx
+++ apps/admin/src/app/schedule/page.jsx
@@ -145,7 +145,7 @@ const testOverLap = blockTime.map(time => {
 
   return startTime > endTime;
 });
- console.log('testing blocktime herfe s', blockTime);
+ console.log('testing blocktime herfe ', blockTime);
 
 
 
