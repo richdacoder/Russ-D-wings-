f231d1c7a71ab75408eca3d4474a270837e041dd
richard williams <richard.williams5697@yahoo.com>
2026-08-26 18:06:57 -0400

    imported convert to eastern

--- apps/admin/src/app/schedule/extras/available.jsx
+++ apps/admin/src/app/schedule/extras/available.jsx
@@ -1,24 +1,20 @@
 "use client";
 
-export default function Available({timeSlot, ConvertToEastern}){
+export default function Available({timeSlot, ConvertToEastern, TimeFormat}){
   console.log('time slot', timeSlot);
 
-const timeFormat = (time) => {
- const actualTime = ConvertToEastern(time, true).split(':');
- return `${actualTime[0]}:${actualTime[1]} ${actualTime[2].split(' ')[1]}`;
-};
 
 return(
   <>
   <div>
     <h1>Start Time </h1>
       {timeSlot.map((time, index) =>(
-        <p key={index}>{timeFormat(time.start_time)}</p>
+        <p key={index}>{TimeFormat(time.start_time)}</p>
       )
       )}
           <h2>End Time </h2>
       {timeSlot.map((time, index) =>(
-        <p key={index}>{timeFormat(time.end_time)}</p>
+        <p key={index}>{TimeFormat(time.end_time)}</p>
       )
       )}
 
--- apps/lib/time-format/display-time.js
+++ apps/lib/time-format/display-time.js
@@ -1,3 +1,5 @@
+ import ConvertToEastern from "./convert-to-eastern";
+
  export default function TimeFormat(time){
  const actualTime = ConvertToEastern(time, true).split(':');
  return `${actualTime[0]}:${actualTime[1]} ${actualTime[2].split(' ')[1]}`;
