cf3a86eca5ff7ffb592f8fbb980dbd8b273576dc
richard williams <richard.williams5697@yahoo.com>
2026-08-26 21:12:07 -0400

    added elements for block time

--- apps/admin/src/app/schedule/extras/block-time.jsx
+++ apps/admin/src/app/schedule/extras/block-time.jsx
@@ -1,9 +1,14 @@
 "use client";
 
 export default function BlockTime({ConvertToEastern, blockTime}){
+  console.log('block time', blockTime)
 return(
   <div>
-
+    {
+    blockTime.map((time, index) => (
+      <p key={index}> {time.start_time} </p>
+    ))
+    }
   </div>
 )
 }
