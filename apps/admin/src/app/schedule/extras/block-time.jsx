e64657ec845c2824af7185fd1e960f32a542714a
richard williams <richard.williams5697@yahoo.com>
2026-08-26 21:17:41 -0400

    start end time element

--- apps/admin/src/app/schedule/extras/block-time.jsx
+++ apps/admin/src/app/schedule/extras/block-time.jsx
@@ -4,11 +4,20 @@ export default function BlockTime({ConvertToEastern, blockTime}){
   console.log('block time', blockTime)
 return(
   <div>
+    <div>
+    <h1>Start Time</h1>
     {
     blockTime.map((time, index) => (
       <p key={index}> {time.start_time} </p>
     ))
     }
+    </div>
+    <div>
+      <h2> End Time </h2>
+      {
+        blockTime.map()
+      }
+    </div>
   </div>
 )
 }
