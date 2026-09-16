e2c73a477c987f0c645f94f4d4ea4e98347b1530
richard williams <richard.williams5697@yahoo.com>
2026-09-15 23:24:14 -0400

    -

--- apps/admin/src/app/schedule/extras/available.jsx
+++ apps/admin/src/app/schedule/extras/available.jsx
@@ -1,15 +1,30 @@
 "use client";
 
+import { useEffect } from 'react';
+
+/*
+9/12/26
+- delete all unwanted time slots
+
+
+*/
+
 export default function Available({timeSlot, setTimeSlot, ConvertToEastern, TimeFormat, Delete}){
-  console.log('time slot in available page', timeSlot[0]?.start_time);
-const unwantedTimeSlot = timeSlot.forEach((value, index) =>{
+  console.log('time slot in available pag', timeSlot[0]?.start_time);
+
+
+  useEffect(async () => {
+  timeSlot.forEach((value, index) =>{
   console.log('value time',value.id, 'index', index);
   if( index !== 0){
     Delete('availability', value.id );
   };
-
 });
-console.log('unwanted time slot', unwantedTimeSlot);
+
+ setTimeSlot(prev => prev.filter((value, index) => index === 0));
+  },
+  []
+);
 
 return(
   <>
