e2fcae266d8af09661ceb7cc5c2f0f94fdfce04f
richard williams <richard.williams5697@yahoo.com>
2026-09-16 17:10:17 -0400

    removed timeslot removal from page

--- apps/admin/src/app/schedule/extras/available.jsx
+++ apps/admin/src/app/schedule/extras/available.jsx
@@ -14,15 +14,6 @@ export default function Available({timeSlot, setTimeSlot, ConvertToEastern, Time
 
 
 
-  timeSlot.forEach((value, index) =>{
-  console.log('value time',value.id, 'index', index);
-  if( index !== 0){
-    // Delete('availability', value.id );
-      console.log('use effect timslot working');
-  };
-});
-
- setTimeSlot(prev => prev.filter((value, index) => index === 0));
 
 return(
   <>
