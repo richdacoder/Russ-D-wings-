8bea707090fe84cc847491d4a093a09daacc968a
richard williams <richard.williams5697@yahoo.com>
2026-09-17 17:21:47 -0400

    added delete

--- apps/lib/time-slot-actions/remove-time-slot.js
+++ apps/lib/time-slot-actions/remove-time-slot.js
@@ -1,8 +1,10 @@
+import Delete from "../delete.js";
+
 export default function RemoveTimeSlot(timeSlot, setTimeSlot){
   timeSlot.forEach((value, index) =>{
   console.log('value time',value.id, 'index', index);
   if( index !== 0){
-    // Delete('availability', value.id );
+     Delete('availability', value.id );
       console.log('use effect timslot working');
   };
 });
