ec8f16f2ed3ba32d99c47dbf604c9a25305c977d
richard williams <richard.williams5697@yahoo.com>
2026-09-23 06:54:49 -0400

    turn query to deleed rows

--- apps/backend/routes/menu.js
+++ apps/backend/routes/menu.js
@@ -84,7 +84,7 @@ console.log('reached delet but not deleted', req);
   try{
     const {id} = req.params;
 
-   await db('menu')
+   const deletedRows = await db('menu')
     .where({id})
     .delete();
 
