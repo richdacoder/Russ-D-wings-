ffa9a3892604ef237293a31c5748e56c53b6a2dd
richard williams <richard.williams5697@yahoo.com>
2026-09-23 08:26:23 -0400

    added drink as a component:

--- apps/admin/src/app/menu/page.jsx
+++ apps/admin/src/app/menu/page.jsx
@@ -155,6 +155,10 @@ setPrice("");
 
               />
             </div>
+            <div>
+              <Drink
+              />
+            </div>
             </div>
         <div>
         </div>
