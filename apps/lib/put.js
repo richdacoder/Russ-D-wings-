9a1b38473110cd5cef70a17db45721340ee3b04f
richard williams <richard.williams5697@yahoo.com>
2026-09-24 08:05:01 -0400

    added p

--- apps/lib/put.js
+++ apps/lib/put.js
@@ -19,7 +19,7 @@ export default async function Put(formatData){
 
   fetch(`${url}/api/${type}/${id}`, {
     method:'PUT',
-    headers: {'Content-Type' : 'aplication/json'},
+    headers: {'Content-Type' : 'application/json'},
     body: `${formatData}`
   }
   )
