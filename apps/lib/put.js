f997f43466119ca46643cb54d777458d647c5c49
richard williams <richard.williams5697@yahoo.com>
2026-09-24 07:35:05 -0400

    changed the way i made id for put

--- apps/lib/put.js
+++ apps/lib/put.js
@@ -2,19 +2,22 @@
 
 - need url
 - make api call
-
+- add type in all databases
 
 
 */
 
 
 
-export default async function Put(formatData, id){
+export default async function Put(formatData){
 
+  const type = formatData.type;
+
+  const id = formatData.id;
 
   const url = process.env.NEXT_PUBLIC_API_URL;
 
-  api(`${url}/api/${}`)
+  api(`${url}/api/${type}/${id}`)
 
 
 }
